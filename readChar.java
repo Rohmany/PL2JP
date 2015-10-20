80d57c4ed2d526b606087ea6ff904756bbeb2d30
Mohammad <mohamadabdalalim@gmail.com>
2015-10-20 19:14:18 +0200

    Create readChar.java

---
+++ readChar.java
@@ -0,0 +1,13 @@
+package mypack;
+import java.util.Scanner;
+
+
+public class MainClass {
+	public static void main(String[] Lolita) {
+		/* Read char */
+		char x;
+		Scanner scan  = new Scanner(System.in);
+		x = scan.next().charAt(0);
+		scan.close();
+	}
+}
